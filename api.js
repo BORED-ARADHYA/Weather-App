c3da83d44dd0163ca24e3158c5ef191d6c80299d
Student Newton School <aradhya.tiwari2024@nst.rishihood.edu.in>
2025-02-15 17:30:32 +0530

    Updated CSS & Fahrenheit Toggle button

--- api.js
+++ api.js
@@ -1,48 +1,57 @@
 const apiKey = "f070765d0b8af070c345f39b388dd495";
-const apiUrl= "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-const searchBox = document.querySelector(".search input")
-const searchBtn = document.querySelector(".search button")
-const weatherIcon = document.querySelector(".weather-icon")
-
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+let isCelsius = true;
 
 async function checkWeather(city) {
-    const response=await fetch(apiUrl + city +`&appid=${apiKey}`);
-    if(response.status == 404){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if (response.status == 404) {
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
-    }
-    else{
+    } else {
         var data = await response.json();
 
-        document.querySelector(".city").innerHTML=data.name;
-        document.querySelector(".temp").innerHTML=Math.round(data.main.temp) +"ºc";
-        document.querySelector(".humidity").innerHTML=data.main.humidity + " %";
-        document.querySelector(".wind").innerHTML=data.wind.speed + " km/h";
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-        if (data.weather[0].main == "Clouds"){
-            weatherIcon.src="images/clouds.png"
-        }
-        else if ((data.weather[0].main == "Clear")) {
-            weatherIcon.src="images/clear.png"
-        }
-        else if ((data.weather[0].main == "Rain")) {
-            weatherIcon.src="images/rain.png"
-        }
-        else if ((data.weather[0].main == "Drizzle")) {
-            weatherIcon.src="images/drizzle.png"
-        }
-        else if ((data.weather[0].main == "Mist")) {
-            weatherIcon.src="images/mist.png"
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "images/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "images/clear.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "images/mist.png";
         }
 
-        document.querySelector(".weather").style.display = "block"
+        document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
-        }
-
+    }
 }
 
-searchBtn.addEventListener("click", ()=>{
+searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
+});
 
-})
+document.getElementById("toggleUnit").addEventListener("click", () => {
+    let tempElement = document.querySelector(".temp");
+    let tempValue = parseInt(tempElement.textContent);
+
+    if (isCelsius) {
+        tempElement.innerHTML = Math.round((tempValue * 9/5) + 32) + "°F";
+        document.getElementById("toggleUnit").innerText = "°C";
+    } else {
+        tempElement.innerHTML = Math.round((tempValue - 32) * 5/9) + "°C";
+        document.getElementById("toggleUnit").innerText = "°F";
+    }
+
+    isCelsius = !isCelsius;
+});
